354b2c4a89ed38ce8d0ad26cbc555dabf946bbab
danimlisto16 <dayanez8@espe.edu.ec>
2021-11-06 13:03:21 -0500

    problema con caraceteres especiales corregidos

--- MediumLinks.py
+++ MediumLinks.py
@@ -3,21 +3,50 @@ import requests
 import pandas as pd
 from icecream import ic
 import pdfkit
+import os
+from datetime import date
+import re
 
-def url2pdf(url,name):
+
+#define class tags to be scraping
+divClass = "hn l"
+h2TitleClass = "az be hp hq hr hs ht hu hv hw hx hy hz ia ib ic id ie if ig ih ii hd hf hg ij hi hj bc"
+h3SummaryClass = "az b fl ep hd ik hf hg ij hi hj ee"
+aLinkClass = "er es et eu ev ew ex ey ez fa fb fc fd fe ff"
+
+# define the name of the directory to be created
+
+def url2pdf(url, name):
+    today = date.today()
+    path = str(today) + "/"
     try:
-        pdfkit.from_url(url,
-                        "./pdfArticles/"+"ARTICLE_"+name + ".pdf")
+        if (not os.path.isdir(path)):
+            os.mkdir(path)
+        pdfkit.from_url(url, path + str(name) + ".pdf")
         return True
     except BaseException as e:
-        print("Error: " ,e)
+        print("Error: ", e)
         return False
 
+
 class article():
-    def __init__(self, title,summary, link):
+    def __init__(self, title, summary, link):
         self.title = title
         self.summary = summary
         self.link = link
+
+
+def cleanTitle(title):
+    s = re.sub('[^A-Za-z0-9]+', ' ', title)
+    return s
+
+
+def checkUrl(url):
+    if url.find("https://") != -1:
+        return url
+    else:
+        return "https://medium.com/" + url
+
 
 #las etiquetass suelen cambiar luego de copiar el link, trabajar
 #con las etiquetas  que aparecen despues de pegar aqui el enlace
@@ -28,23 +57,42 @@ soup = BeautifulSoup(webpage, "html.parser")
 articles = []
 
 i = 1
-for divs in soup.find_all("div", id=False, class_="hk l"): #get all articles from web called ae fm
+for divs in soup.find_all(
+        "div", id=False,
+        class_=divClass):  #get all articles from web called ae fm
     try:
-        title = divs.findChildren("h2" ,recursive = True ,id=False ,class_="az be hm hn ho hp hq hr hs ht hu hv hw hx hy hz ia ib ic id ie if hc he hf ig hh hi bc")
+        title = divs.findChildren(
+            "h2",
+            recursive=True,
+            id=False,
+            class_=
+            h2TitleClass
+        )
+        
         for child in title:
-            titleText = child.string #get text in h2    
-        summary = divs.findChildren("h3" ,recursive = True ,id=False ,class_="az b fl ep hc ih he hf ig hh hi ee")
+            titleText = child.string  #get text in h2
+        summary = divs.findChildren(
+            "h3",
+            recursive=True,
+            id=False,
+            class_=h3SummaryClass)
+        
         for child in summary:
-            summaryText = child.string #get text in h3
-        link = divs.findChildren("a",recursive = True, id=False, class_="er es et eu ev ew ex ey ez fa fb fc fd fe ff",href = True)
-        for child in link: 
-            a = child #get all a tag in div
+            summaryText = child.string  #get text in h3
+        link = divs.findChildren(
+            "a",
+            recursive=True,
+            id=False,
+            class_=aLinkClass,
+            href=True)
+        for child in link:
+            a = child  #get all a tag in div
             #print("enlace ",a['href'],"\n")
         linkText = a['href']
-        
-        articleObj = article(titleText,summaryText, linkText)
+
+        articleObj = article(titleText, summaryText, linkText)
         articles.append(articleObj)
-        
+
     except BaseException as e:
         print(e)
 
@@ -53,17 +101,23 @@ correct = 0
 wrong = 0
 
 #TODO extract 2 a Tags cause one of them brings the correct link
-#TODO find other way to download webPDF  
-
-for i in articles: 
-    print("================================= ITEM =====================================")
-    print("Título:  "+ i.title)
-    print("Resumen: "+ i.summary)
+#TODO find other way to download webPDF
+iterador = 0
+for i in articles:
+    print(
+        "================================= ITEM ====================================="
+    )
+    print("Título:  " + i.title)
+    print("Resumen: " + i.summary)
     print("Enlace: " + i.link)
-    print("============================================================================")
-    if(url2pdf(i.link,i.title)):
+    print(
+        "============================================================================"
+    )
+    if (url2pdf(checkUrl(i.link), cleanTitle(i.title))):
         correct = correct + 1
     else:
         wrong = wrong + 1
+    iterador = iterador + 1
 print(".......REPORT.......")
-print("CORRECT: "+ str(correct), "WRONG: "+ str(wrong),"TOTAL: "+ str(totalItems))
+print("CORRECT: " + str(correct), "\nWRONG: " + str(wrong),
+      "\nTOTAL: " + str(totalItems))
